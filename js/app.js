5cd05b4d6855148503db3b8ceb7e374adb20f2cc
MichelangelaPaesano <paesanomichelangela@gmail.com>
2025-06-09 17:59:58 +0200

    JS

---
+++ js/app.js
@@ -0,0 +1,43 @@
+//la prima cosa da fare è recuperare tutti i campi del form dove l'utente inserisce i dati 
+const form = document.getElementById ('preventivoForm')
+const name = document.getElementById ('inputName')
+const surname = document.getElementById ('inputSurname')
+const email = document.getElementById ('inputEmail')
+const lavoro = document.getElementById ('inputLavoro')
+const note = document.getElementById ('inputNote')
+const promotion = document.getElementById ('inputCode')
+const formBusiness = document.getElementById ('prezzo')
+
+//seconda cosa dare fare è andare a fare la funzione, ma con attenzione
+//essendoci il form e quindi si deve fare button 
+form.addEventListener ('submit', function (event) {
+    event.preventDefault()
+    //adesso recuperiamo i valori inseriti nel form 
+    const inputName = name.value
+    const inputSurname = surname.value
+    const inputNote = note.value
+    const inputCode = promotion.value
+    const inputLavoro = lavoro.value
+
+    //creo una variabile per le ore 
+    const inputOre = 10
+
+    //creo una variabile per il prezzo orario dei singoli lavori
+    const prezziOrari = {
+        frontend: 15.30,
+        backend: 20.50,
+        analisi: 33.60
+    }
+
+    //qui creo il prezzo finale
+    if (prezziOrari[inputLavoro]) {
+        const prezzofinale = prezziOrari[inputLavoro] * inputOre
+         console.log(`Il prezzo finale per il lavoro di ${inputJob} è: ${prezzofinale}€`);
+    } else {
+        console.log('tipo di lavoro non valido')
+    }
+
+    //adesso i codici sconto e come applicarli 
+    
+
+})
